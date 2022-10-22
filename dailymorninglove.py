135fa502d1d736a1095e318b4717b98aff470bbd
GitHubguoliqiang <49651249+GitHubguoliqiang@users.noreply.github.com>
2022-10-22 16:05:06 +0800

    Update dailymorninglove.py

--- dailymorninglove.py
+++ dailymorninglove.py
@@ -4,7 +4,7 @@ def checkSignature(data):
     nonce = data.get("nonce")
     if not signature or not timestamp or not nonce:
         return False
-    tmp_str = "".join(sorted([TOKEN, timestamp, nonce]))
+    tmp_str = "guoguohefeifeidailymorninglove".join(sorted([TOKEN, timestamp, nonce]))
     tmp_str = hashlib.sha1(tmp_str.encode('UTF-8')).hexdigest()
     if tmp_str == signature:
         return True
